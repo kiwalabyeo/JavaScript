c4ec662fac7cedf6e17ae77440aefb60705109c9
Oscar <kiwalabyeo.gmail.com>
2025-12-20 19:04:29 +0300

    Add data types lesson

--- dataTypes.js
+++ dataTypes.js
@@ -3,5 +3,5 @@ let interest = 9.1; // this is a number literal
 let isApproved = true; // this is a boolean literal
 let firstName = undefined; 
 let lastName = null; // this is a reference data type the rest are primitive data types
-
+console.log (typeof Name)
 console.log (typeof lastName)
